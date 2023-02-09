4c57bb8ac0096f645a8322569cf31bd981a22d00
WagnerMor <wagnermor@outlook.com>
2023-02-09 10:24:46 -0300

    Fix: Add new Query Method

--- src/main/java/org/generation/blog_pessoal/repository/UsuarioRepository.java
+++ src/main/java/org/generation/blog_pessoal/repository/UsuarioRepository.java
@@ -1,14 +1,16 @@
 package org.generation.blog_pessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.blog_pessoal.model.Usuarios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuarios, Long> {
 	public Optional<Usuarios> findByUsuario(String usuario);
-	
+	public List <Usuarios> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
